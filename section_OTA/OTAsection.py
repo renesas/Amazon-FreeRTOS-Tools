2d645e666706d1320e436193f8b3e685dda7ca84
Dinh Van Nam <vannam.dinh.xt@renesas.com>
2020-10-06 15:01:24 +0900

    Change to OTA demo configuration in many cases of Renesas folder structure
    Signed-off-by: Dinh Van Nam <vannam.dinh.xt@renesas.com>

--- section_OTA/OTAsection.py
+++ section_OTA/OTAsection.py
@@ -10,21 +10,27 @@ import argparse
 parser = argparse.ArgumentParser(description='Change config to OTA demo and the section')
 parser.add_argument('--path-aws', action='store', dest='path_aws',
                     help='Store the path to amazon-freertos')
+parser.add_argument('--path-project', action='store', dest='path_project',
+                    help='Path to project')
 args = parser.parse_args()
 
-config_file			= str(args.path_aws) + '//amazon-freertos//vendors//renesas//boards//rx65n-rsk//aws_demos//config_files//aws_demo_config.h'
-cproject_file       = str(args.path_aws) +  '//amazon-freertos//projects//renesas//rx65n-rsk//e2studio//aws_demos//.cproject'
+config_file			= str(args.path_aws) + '//amazon-freertos//vendors//renesas//boards//rx_mcu_boards//' + str(args.path_project) + '//aws_demos//config_files//aws_demo_config.h'
+cproject_file       = str(args.path_aws) +  '//amazon-freertos//projects//renesas//' + str(args.path_project) + '//e2studio//aws_demos//.cproject'
 
 is_config_file = os.path.isfile(config_file)
 
 if is_config_file is False:
-    config_file = str(args.path_aws) + '//amazon-freertos//vendors//renesas//rx_mcu_boards//boards//rx65n-rsk//aws_demos//config_files//aws_demo_config.h'
+    config_file = str(args.path_aws) + '//amazon-freertos//vendors//renesas//rx_mcu_boards//boards//' + str(args.path_project) + '//aws_demos//config_files//aws_demo_config.h'
+    is_config_file = os.path.isfile(config_file)
+
+if is_config_file is False:
+    config_file = str(args.path_aws) + '//amazon-freertos//vendors//renesas//boards//' + str(args.path_project) + '//aws_demos//config_files//aws_demo_config.h'
 
 """Read aws_demo_config.h """
 read_config_file = open(config_file, "r")
 new_config_file = ""
 for line in read_config_file:
-    new_line = line.strip().replace("CONFIG_MQTT_DEMO_ENABLED", "CONFIG_OTA_UPDATE_DEMO_ENABLED")
+    new_line = line.strip().replace("#define CONFIG_MQTT_DEMO_ENABLED", "#define CONFIG_OTA_UPDATE_DEMO_ENABLED")
     new_config_file += new_line +"\n"
 read_config_file.close()
 
